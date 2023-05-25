2f2575239115bd3dfc46cd7aa983ecebab868cb7
[aiju.kawase] <[aiju.kawase@appirits.com]>
2023-05-25 14:55:58 +0900

    migrate, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'site/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    resources :events, only: %i[index show create destroy update]
+  end
 end
